b7b620192d56103bafc0a6dfe390b83aed4a4cf3
unknown <topolewr@cooley.edu>
2022-02-26 11:55:18 -0500

    ajusted the speeds  for the ConveyorMove and ShooterMoveLow

--- src/main/java/frc/robot/commands/ConveyorMove.java
+++ src/main/java/frc/robot/commands/ConveyorMove.java
@@ -54,7 +54,7 @@ public class ConveyorMove extends CommandBase {
     // Called every time the scheduler runs while the command is scheduled.
     @Override
     public void execute() {
-        m_conveyor.movePower(0.3);
+        m_conveyor.movePower(0.4);
     }
 
     // Called once the command ends or is interrupted.
